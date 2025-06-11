31790bc15b6cc6aafc224563af0c5be6ca49741e
happymuchsin <happymuchsin@gmail.com>
2025-06-11 10:57:55 +0700

    v1.1.9

--- lib/app/controllers/approval_controller.dart
+++ lib/app/controllers/approval_controller.dart
@@ -3,14 +3,18 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
 import 'package:nemo/app/data/models/approval_model.dart';
 import 'package:nemo/app/ui/global_widgets/button.dart';
+import 'package:nemo/app/ui/global_widgets/decoration.dart';
+import 'package:nemo/app/ui/global_widgets/fixed_form.dart';
 import 'package:nemo/app/ui/global_widgets/helper_screen.dart';
 import 'package:nemo/app/ui/global_widgets/needle.dart';
 import 'package:nemo/app/ui/global_widgets/notif.dart';
 import 'package:nemo/app/ui/utils/api.dart';
+import 'package:nemo/app/ui/utils/global_context.dart';
 import 'package:nemo/app/ui/utils/local_data.dart';
 
 class ApprovalController extends GetxController {
@@ -27,6 +31,7 @@ class ApprovalController extends GetxController {
       timeScanBox = "".obs;
   var fIdCard = FocusNode(), fBoxCard = FocusNode();
   var lIdCard = [].obs, lBoxCard = [].obs;
+  late AwesomeDialog hDialog;
 
   @override
   void onReady() {
@@ -142,7 +147,7 @@ class ApprovalController extends GetxController {
       EasyLoading.dismiss();
       xdialog.dismiss();
       fIdCard.unfocus();
-      scanBoxCard();
+      historyNeedle(r['data']['history']);
     } else {
       timeScanRfid('');
       timeScanBox('');
@@ -151,7 +156,77 @@ class ApprovalController extends GetxController {
     }
   }
 
-  Future<void> scanBoxCard() async {
+  Future<void> historyNeedle(list) async {
+    hDialog = AwesomeDialog(
+      context: GlobalService.navigatorKey.currentState!.overlay!.context,
+      animType: AnimType.scale,
+      dialogType: DialogType.noHeader,
+      keyboardAware: true,
+      showCloseIcon: true,
+      closeIcon: const Icon(FontAwesomeIcons.x),
+      dismissOnTouchOutside: false,
+      body: Column(
+        children: [
+          modalTitle(text: 'History Needle'),
+          Row(
+            children: [
+              Expanded(flex: 2, child: Text('Brand', style: TextStyle(fontSize: deviceType.value == 'tablet' ? 30 : 18))),
+              Expanded(flex: 3, child: Text('Type', style: TextStyle(fontSize: deviceType.value == 'tablet' ? 30 : 18))),
+              Expanded(flex: 1, child: Text('Size', style: TextStyle(fontSize: deviceType.value == 'tablet' ? 30 : 18))),
+              Expanded(flex: 4, child: Text('Code', style: TextStyle(fontSize: deviceType.value == 'tablet' ? 30 : 18))),
+              Expanded(flex: 3, child: Text('Date Time', style: TextStyle(fontSize: deviceType.value == 'tablet' ? 30 : 18))),
+            ],
+          ),
+          ListView.builder(
+            physics: const NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            itemCount: list.length,
+            itemBuilder: (context, index) {
+              final data = list[index];
+              return Row(
+                children: [
+                  Expanded(flex: 2, child: Text(data['brand'].toString(), style: TextStyle(fontSize: deviceType.value == 'tablet' ? 25 : 14))),
+                  Expanded(flex: 3, child: Text(data['tipe'].toString(), style: TextStyle(fontSize: deviceType.value == 'tablet' ? 25 : 14))),
+                  Expanded(flex: 1, child: Text(data['size'].toString(), style: TextStyle(fontSize: deviceType.value == 'tablet' ? 25 : 14))),
+                  Expanded(flex: 4, child: Text(data['code'].toString(), style: TextStyle(fontSize: deviceType.value == 'tablet' ? 25 : 14))),
+                  Expanded(flex: 3, child: Text(data['created_at'].toString(), style: TextStyle(fontSize: deviceType.value == 'tablet' ? 25 : 14))),
+                ],
+              );
+            },
+          ),
+          headerFile('Change Needle', paddingTop: 0, paddingBottom: 0),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+            child: Row(
+              children: [
+                exBtn(
+                    type: 'row',
+                    onPressed: () => scanBoxCard('ubah'),
+                    backgroundColor: Colors.green,
+                    isIcon: true,
+                    icon: FontAwesomeIcons.check,
+                    isText: deviceType.value == 'tablet' ? true : false,
+                    text: deviceType.value == 'tablet' ? 'Yes' : ''),
+                const SizedBox(
+                  width: 20,
+                ),
+                exBtn(
+                    type: 'row',
+                    onPressed: () => scanBoxCard('tetap'),
+                    backgroundColor: Colors.red,
+                    isIcon: true,
+                    icon: FontAwesomeIcons.x,
+                    isText: deviceType.value == 'tablet' ? true : false,
+                    text: deviceType.value == 'tablet' ? 'No' : ''),
+              ],
+            ),
+          ),
+        ],
+      ),
+    )..show();
+  }
+
+  Future<void> scanBoxCard(mode) async {
     Future.delayed(const Duration(milliseconds: 50), () {
       fBoxCard.requestFocus();
     });
@@ -169,7 +244,7 @@ class ApprovalController extends GetxController {
 
                 if (!value) {
                   if (sBoxCard.value != '') {
-                    await scanBox();
+                    await scanBox(mode);
 
                     sBoxCard.value = '';
                     fBoxCard.requestFocus();
@@ -177,7 +252,7 @@ class ApprovalController extends GetxController {
                 }
               },
               txt: sBoxCard.value),
-          cardScan('Scan New Box Card'),
+          cardScan(mode == 'ubah' ? 'Scan Change Needle Box Card' : 'Scan Box Card'),
         ],
       ),
       onDismissCallback: (p0) {
@@ -201,24 +276,26 @@ class ApprovalController extends GetxController {
     return event.logicalKey == LogicalKeyboardKey.enter ? KeyEventResult.handled : KeyEventResult.ignored;
   }
 
-  Future<void> scanBox() async {
+  Future<void> scanBox(mode) async {
     timeScanBox(DateFormat('yyyy-MM-dd HH:mm:ss').format(DateTime.now()));
     EasyLoading.show();
     Map<String, dynamic> data = {};
     data['rfid'] = sBoxCard.value.toString();
     if (kDebugMode) {
-      data['rfid'] = '00box1';
+      data['rfid'] = '00box2n';
     }
     data['tipe'] = 'approval';
     data['approval'] = sApproval.value;
     data['username'] = await localShared.baca('username');
     data['scan_rfid'] = timeScanRfid.value;
     data['scan_box'] = timeScanBox.value;
+    data['mode'] = mode;
     var a = await apiReq.baseUrl();
     var r = await apiReq.makeRequest('$a/card/box', data);
     if (r['success'] == 200) {
       EasyLoading.dismiss();
       xdialog.dismiss();
+      hDialog.dismiss();
       notif(
         r['message'],
         tipe: 'success',
